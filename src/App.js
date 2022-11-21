25897f588ae1f572a5c1f75d3c680fc6dc64a1f9
jede <jedebrahma@pursuit.org>
2022-11-20 22:33:37 -0500

    get notifications

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
-
-import './App.css';
+import "./App.css";
 // import messaging  from "../src/Firebase/firebase-messaging-sw"
 // import firebase from "../src/Firebase/firebase"
-import {retrieveToken} from "../src/Firebase/firebase"
-import Notification from './Notification';
+import { retrieveToken } from "../src/Firebase/firebase";
+import Notification from "./Notification";
 
 function App() {
-  
   return (
-    
     <div>
-
-
-   <h1>Welcome to WellBell!</h1>
-   <Notification/>
-  </div>
+      <h1>Welcome to WellBell!</h1>
+      <Notification />
+    </div>
   );
 }
 
