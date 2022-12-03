084aaec3525c5e9f5235f8386b028ebe4d9829cc
Zane Ahmed <zaneahmed4@gmail.com>
2022-12-02 20:44:37 -0500

    adds toggle

--- src/Components/UserInput/UserIntakeForm.js
+++ src/Components/UserInput/UserIntakeForm.js
@@ -1,20 +1,43 @@
-import React from 'react'
+import React, {useState, useEffect } from "react";
+import axios from "axios"
 
 function UserIntakeForm() {
+
+const[reminderType, setReminderType]= useState("")
+const [toggle, setToggle] = useState(false);
+
+const handleClick = (e) => {
+  e.preventDefault();
+  setReminderType(e.target.value)
+  setToggle(!toggle);
+}
+
+const resetRadioState = () => {
+  setReminderType('');
+}
+
+
   return (
     <div>
         <h2>Continue Your Wellness Profile</h2>
-        <form>
-            <p>Select a Username</p>
+        <form >
+            {/* <p>Select a Username</p>
             <input type="text" required placeholder="Username"></input>
-            <br/>
-            <p>Select the types of reminders you wish to receive</p>
-            <input type="radio" id="physical" value="physical"></input>
-            <label for="physical">Physical Wellness</label>
-            <input type="radio" id="nutrition" value="nutrition"></input>
-            <label for="nutrition">Nutritional Wellness</label>
-            <input type="radio" id="mental" value="mental"></input>
-            <label for="mental">Mental Wellness</label>
+            <br/> */}
+
+            <p>Select the type of reminders you wish to receive</p>
+            <input type="radio"  id="physical" onChange={handleClick} checked={toggle} value="physical"></input>
+            <label htmlFor="physical">Physical Wellness</label>
+
+            <input type="radio" id="nutrition" onChange={handleClick} checked={reminderType === "Nutrition"} value="nutrition"></input>
+            <label htmlFor="nutrition">Nutritional Wellness</label>
+
+            <input type="radio"  id="mental" onChange={handleClick} checked={reminderType === "Mental Wellness"} value="mental"></input>
+            <label htmlFor="mental">Mental Wellness</label>
+
+        <button  type="reset"
+          onClick={resetRadioState}>RESET</button>
+            <p>{reminderType}</p>
         </form>
     </div>
   )
