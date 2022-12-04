de08e0d6ad15f42fade3b97d8a77d5f168824d8a
Zane Ahmed <zaneahmed4@gmail.com>
2022-12-04 12:51:28 -0500

    style icons

--- src/Components/Dashboard/DashNav.js
+++ src/Components/Dashboard/DashNav.js
@@ -10,12 +10,12 @@ function DashNav() {
   return (
     <div className='dash-nav-main'>
 
-    <img onClick={()=>navigate("/dashboard")} src={logo}/>
+    <img onClick={()=>navigate("/dashboard")} src={logo} className="logo"/>
     <span className='user-dash-greeting'>Welcome User</span>
     <i onClick={()=>navigate("/dashboard")} className="dash-icon fa-solid fa-house"></i>
     <i onClick={()=>navigate("/dashboard/bells")} className="dash-icon fa-regular fa-bell"></i>
     <i onClick={()=>navigate("/dashboard/rewards")} className="dash-icon fa-solid fa-award"></i>
-    <i onClick={()=>navigate("/about")} className="dash-icon fa-solid fa-circle-info"></i>
+    <i onClick={()=>navigate("/about")} className="dash-icon-info fa-solid fa-circle-info"></i>
     </div>
   )
 }
