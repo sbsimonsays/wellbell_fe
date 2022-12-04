3bad670bb3e6d8b6f120ec381f5bf404283a169f
jede <jedebrahma@pursuit.org>
2022-12-04 12:34:01 -0500

    add dashboard icons

--- src/Components/Dashboard/DashNav.js
+++ src/Components/Dashboard/DashNav.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import "./DashNav.css"
+import logo from "../../public/wellbell.png"
+import { useNavigate } from "react-router-dom"
+
 
 
 function DashNav() {
+  const navigate =  useNavigate() 
   return (
     <div className='dash-nav-main'>
 
+    <img onClick={()=>navigate("/dashboard")} src={logo}/>
+    <span className='user-dash-greeting'>Welcome User</span>
+    <i onClick={()=>navigate("/dashboard")} className="dash-icon fa-solid fa-house"></i>
+    <i onClick={()=>navigate("/dashboard/bells")} className="dash-icon fa-regular fa-bell"></i>
+    <i onClick={()=>navigate("/dashboard/rewards")} className="dash-icon fa-solid fa-award"></i>
+    <i onClick={()=>navigate("/about")} className="dash-icon fa-solid fa-circle-info"></i>
     </div>
   )
 }
