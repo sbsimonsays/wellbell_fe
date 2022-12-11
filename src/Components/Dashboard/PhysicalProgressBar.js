e9c65342cd6720c8e4e6d4d00d840ce91bb73112
Spencer Simon <spencer.san@gmail.com>
2022-12-11 14:10:25 -0500

    added bars and changed profile.css

---
+++ src/Components/Dashboard/PhysicalProgressBar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ".././Dashboard/PhysicalProgressBar.css";
+
+export default function PhysicalProgressBar({ progress }) {
+  return (
+    <div className="container">
+      <div
+        className="pbar"
+        style={{ width: `${progress}%` }}
+        role="progressbar"
+        aria-valuenow={progress}
+        aria-valuemin={0}
+        aria-valuemax={100}
+        aria-width={progress}
+      >
+        <span className="label">{`${progress}%`}</span>
+      </div>
+    </div>
+  );
+}
---
+++ src/Components/Dashboard/SelfCareProgressBar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ".././Dashboard/SelfCareProgressBar.css";
+
+export default function SelfCareProgressBar({ progress }) {
+  return (
+    <div className="container">
+      <div
+        className="sbar"
+        style={{ width: `${progress}%` }}
+        role="progressbar"
+        aria-valuenow={progress}
+        aria-valuemin={0}
+        aria-valuemax={100}
+        aria-width={progress}
+      >
+        <span className="label">{`${progress}%`}</span>
+      </div>
+    </div>
+  );
+}
